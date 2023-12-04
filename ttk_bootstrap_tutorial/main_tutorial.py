da12424b6d84454720d9cc15de1857225538d6e4
Chase Gravett <garrisongravett@gmail.com>
2023-12-03 22:02:32 -0600

    file changes

---
+++ ttk_bootstrap_tutorial/main_tutorial.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import ttkbootstrap as ttkb
+from ttkbootstrap.constants import *
+
+# Lots of cool themes available. This one looks cool to me
+root = ttkb.Window(themename="solar")
+
+root.title("Bootstrap for desktop? WITH PYTHON?!")
+root.geometry("500x500")
+
+root.mainloop()
