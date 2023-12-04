c6fc1b087dda9f445bdd9f44fcf4714451cef004
Chase Gravett <garrisongravett@gmail.com>
2023-12-04 17:46:10 -0600

    Fixed interpreter issues

--- ttk_bootstrap_tutorial/main_tutorial.py
+++ ttk_bootstrap_tutorial/main_tutorial.py
@@ -1,9 +1,9 @@
 import tkinter as tk
-import ttkbootstrap as ttkb
+from ttkbootstrap import *
 from ttkbootstrap.constants import *
 
 # Lots of cool themes available. This one looks cool to me
-root = ttkb.Window(themename="solar")
+root = Window(themename="solar")
 
 root.title("Bootstrap for desktop? WITH PYTHON?!")
 root.geometry("500x500")
